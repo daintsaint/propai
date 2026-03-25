e9e86d19a53681994b06db0387d2d38520ae419e
daintsaint <163385716+daintsaint@users.noreply.github.com>
2026-03-25 15:57:43 +0800

    Add web routes

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\FaqController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/faq', [FaqController::class, 'index'])->name('faq');
